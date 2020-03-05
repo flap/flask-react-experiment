be040c6b144635b9ac601adf10d8c23c740d245c
Lilian <cavalet.lilian@gmail.com>
2020-03-05 00:40:31 -0300

    Fix modal to apear after search

--- ui/src/components/AnimatedModal.js
+++ ui/src/components/AnimatedModal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
@@ -19,43 +18,32 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function AnimatedModal() {
+export default function AnimatedModal(props) {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
 
     return (
         <div>
-
-            <Button variant="contained" color="secondary" onClick={handleOpen}>
-                Open Animated Modal
-            </Button>
-
             <Modal
+                {...props}
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
                 className={classes.modal}
-                open={open}
-                onClose={handleClose}
                 closeAfterTransition
                 BackdropComponent={Backdrop}
                 BackdropProps={{
                     timeout: 500,
                 }}
             >
-            <Fade in={open}>
+            <Fade in={props.open}>
                     <div className={classes.paper}>
-                        <h2>Animated React Modal</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi accumsan odio enim.
-                        </p>
+                        <h1>{props.content}</h1>
+                    {/* {this.state.apiResult.map((apiResult, index) => (
+                        <div key={index}>
+                            <h1 key={apiResult.idCardNumber.toString()}>{apiResult.idCardNumber}</h1>
+                            <h1 key={apiResult.cpfNumber.toString()}>{apiResult.cpfNumber}</h1>
+                            <h1 key={apiResult.name}>{apiResult.name}</h1>
+                        </div>
+                    ))} */}
                     </div>
                 </Fade>
             </Modal>
