95d8cf9fdb4ce9599ce678adff673b25986a0b99
liliancavalet <cavalet.lilian@gmail.com>
2020-07-18 21:29:31 -0300

    Add POST endpoint to create the players list

--- turn-tracker/routes.py
+++ turn-tracker/routes.py
@@ -1,11 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/circular')
-def hello_world():
-    return jsonify('Hello, World!')
+@app.route('/circular', methods=['GET', 'POST'])
+def circular_list():
+    if request.method == 'POST':
+        players = []
+        [players.append(player) for player in request.json]
+        return jsonify(players)
+    else:
+        # You probably don't have args at this route with GET
+        # method, but if you do, you can access them like so:
+        # yourarg = flask.request.args.get('argname')
+        # your_register_template_rendering(yourarg)
+        return jsonify('Hello, World!')
 
 
 if __name__ == '__main__':
