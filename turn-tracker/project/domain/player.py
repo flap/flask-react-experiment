ee5e0094e809584d2070b5d3de562c2cf013c6e8
liliancavalet <cavalet.lilian@gmail.com>
2021-09-07 19:42:57 -0300

    Add Player model with dict conversion

--- turn-tracker/project/domain/player.py
+++ turn-tracker/project/domain/player.py
@@ -1,4 +1,9 @@
-class Player:
+from collections import namedtuple
+
+from project.domain.person import Person
+
+
+class Player(Person):
     """
     Create the object palyer. A player must have:
         - name
@@ -10,10 +15,8 @@ class Player:
     """
 
     def __init__(self, name, date_of_birth, is_associate, position, is_host):
-        self.name = name
-        self.date_of_birth = date_of_birth,
-        self.is_associate = is_associate,
-        self.position = position,
+        super().__init__(name, date_of_birth, is_associate)
+        self.position = position
         self.is_host = is_host
 
     def __eq__(self, other):
@@ -22,3 +25,11 @@ class Player:
                    self.is_associate == other.is_associate and self.position == other.position and \
                    self.is_host == other.is_host
         return False
+
+    @staticmethod
+    def from_dict(d):
+        return namedtuple("Player", d.keys())(*d.values())
+
+    def to_dict(self):
+        return {'name': self.name, 'date_of_birth': self.date_of_birth, 'is_associate': self.is_associate,
+                'position': self.position, 'is_host': self.is_host}
